d1aa47fcdf8492568462f389a240b7fd05cd4369
Hiroshi Hagiwara <mukakinkatsubou@gmail.com>
2025-03-15 12:34:29 +0900

    refactor:

---
+++ src/common.ts
@@ -0,0 +1,12 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+export async function axiosRequest<T>(requestOptions: AxiosRequestConfig) {
+    return axios(requestOptions)
+        .then((res: AxiosResponse<T>) => {
+            return res.data;
+        })
+        .catch((e: AxiosError<{ error: string; }>) => {
+            console.log(e.message);
+        }
+    );
+}
---
+++ src/const.ts
@@ -0,0 +1,7 @@
+export const HOYOLAB_DAILY_API_URL = 'https://bbs-api-os.hoyoverse.com/game_record/genshin/api/dailyNote';
+export const REPORT_BORDER_RESIN_RECOVERY_TIME = 7200;
+export const REPORT_BORDER_HOME_COIN_RECOVERY_TIME = 36000;
+export const DAILY_REWARD_NOTIFY_OCLOCK = 21;
+export const MAX_RESIN = 200;
+export const MAX_HOME_COIN = 2400;
+export const DAILY_TASK_NUMBER = 4;
